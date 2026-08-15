c2a0b944978a0ded89d39a7c76e8410093d63329
jorgebbastos <161392714+jorgebbastos@users.noreply.github.com>
2026-08-14 22:04:03 -0300

    Enhance calcular_total with coupon support
    Added coupon functionality to calcular_total function.

--- calculadora.py
+++ calculadora.py
@@ -1,18 +1,28 @@
-def calcular_total(itens, desconto_percentual=0):
-    """
-    Calcula o total de uma compra.
+def calcular_total(itens, desconto_percentual=0, cupom=""):
 
-    Cada item representa uma tupla no formato:
-    (preco_unitario, quantidade)
-    """
     if not 0 <= desconto_percentual <= 100:
         raise ValueError("O desconto precisa estar entre 0 e 100.")
+
+    cupons = {
+        "vale10": 10,
+        "vale5": 5
+    }
+
+    if cupom == "":
+        desconto_cupom = 0
+    elif cupom in cupons:
+        desconto_cupom = cupons[cupom]
+    else:
+        raise ValueError("Cupom inválido.")
+
+    if desconto_percentual + desconto_cupom > 100:
+        raise ValueError("Desconto total maior que 100%")
 
     subtotal = sum(
         preco_unitario * quantidade
         for preco_unitario, quantidade in itens
     )
 
-    total = subtotal - desconto_percentual
-    #teste
+    total = subtotal - (subtotal * ((desconto_percentual + desconto_cupom) / 100))
+
     return round(total, 2)
